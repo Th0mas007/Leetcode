e7ad0d635ca7d7bcb7afb4aa7e4cdf75e2d6bbe3
Th0mas007 <116565740+Th0mas007@users.noreply.github.com>
2022-10-24 18:01:32 +0530

    Update Design Hashset.py

--- Linked List/Design Hashset.py
+++ Linked List/Design Hashset.py
@@ -3,37 +3,21 @@ class ListNode:
         self.key=key
         self.val=val
         self.next=None
-class MyHashMap:
+class MyHashSet:
 
     def __init__(self):
         self.size=1000
-        self.m=[None]*self.size
+        self.m=[False]*self.size
         
 
-    def put(self, key: int, val: int) -> None:
+    def add(self, key: int) -> None:
         index=key%self.size
         if self.m[index] is None:
-            self.m[index]=ListNode(key,val)
+            self.m[index]=ListNode(key,True)
         else:
             currNode=self.m[index]
-            self.m[index]=ListNode(key,val)
+            self.m[index]=ListNode(key,True)
             self.m[index].next=currNode
-        
-
-    def get(self, key: int) -> int:
-        index=key%self.size
-        if self.m[index] is None:
-            return -1
-        else:
-            currNode=self.m[index]
-            while currNode:
-                if currNode.key==key:
-                    if currNode.val is None:
-                        return -1
-                    else:
-                        return currNode.val
-                currNode=currNode.next
-            return -1
         
 
     def remove(self, key: int) -> None:
@@ -44,13 +28,30 @@ class MyHashMap:
             currNode=self.m[index]
             while currNode:
                 if currNode.key==key:
-                    currNode.val=None
+                    currNode.val=False
                     break
-                currNode=currNode.next        
+                currNode=currNode.next
+
+    def contains(self, key: int) -> bool:
+        index=key%self.size
+        if self.m[index] is None:
+            return False
+        else:
+            currNode=self.m[index]
+            while currNode:
+                if currNode.key==key:
+                    if currNode.val==True:
+                        return True
+                    else:
+                        return False
+                currNode=currNode.next
+            return False
+                        
+        
 
 
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
 # obj.remove(key)
+# param_3 = obj.contains(key)
