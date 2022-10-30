788d8753a16c52a0570a3b8618905ea4355114db
Th0mas007 <116565740+Th0mas007@users.noreply.github.com>
2022-10-30 22:04:16 +0530

    Create 395. Longest Substring with At Least K Repeating Characters.py

---
+++ Sliding Window/395. Longest Substring with At Least K Repeating Characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        problem_letters=[]
+        flag=True
+        counter=Counter(s)
+        for letter in counter:
+            if counter[letter]<k:
+                problem_letters.append(letter)
+                flag=False
+        if flag:
+            return len(s)
+        
+        for letter in problem_letters:
+            s=s.replace(letter,' ')
+        strings_after_divide=s.split()
+        ans=0
+        for string in strings_after_divide:
+            ans=max(ans,self.longestSubstring(string,k))
+        return ans
+        
